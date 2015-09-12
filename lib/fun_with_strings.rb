493d469119d70c1b84a75b9a7712e9ff233bb608
nikita <nikita_0208@tamu.edu>
2015-09-12 18:17:27 -0500

    palindrome function passes tests

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,6 +1,7 @@
 module FunWithStrings
   def palindrome?
     # returns true if the string is a palindrome.
+    self.downcase.gsub(/[^a-z]/, '') == self.reverse.downcase.gsub(/[^a-z]/, '')
   end
   def count_words
     # your code here
