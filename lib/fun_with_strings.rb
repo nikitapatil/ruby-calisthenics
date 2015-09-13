22ef25000c8a915869a3611586746cef6bd7a4fd
nikita <nikita_0208@tamu.edu>
2015-09-12 20:08:34 -0500

    word count passes all tests

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -4,7 +4,18 @@ module FunWithStrings
     self.downcase.gsub(/[^a-z]/, '') == self.reverse.downcase.gsub(/[^a-z]/, '')
   end
   def count_words
-    # your code here
+    # returns a hash of words in a string and the number of times they occur.
+    #word_hash = {"Hello"=>1, "world"=>2}
+    word_array = self.downcase.gsub(/[^a-z ]/, '').split
+    word_hash = {}
+    word_array.each do |word|
+      if word_hash.key?(word)
+        word_hash[word] += 1
+      else
+        word_hash[word] = 1
+      end
+    end
+    return word_hash
   end
   def anagram_groups
     # your code here
--- spec/fun_with_strings_spec.rb
+++ spec/fun_with_strings_spec.rb
@@ -12,10 +12,10 @@ describe 'palindrome detection' do
   it 'should ignore nonword characters [10 points]' do
     'A man, a plan, a canal -- Panama'.should be_a_palindrome
     "Madam, I'm Adam!".should be_a_palindrome
-  end    
+  end
 end
 
-describe 'word count', :pending => true do
+describe 'word count' do
   it 'should return a hash [5 points]' do
     'now is the time'.count_words.should be_a_kind_of Hash
   end
@@ -56,5 +56,5 @@ describe 'anagram grouping', :pending => true do
         @anagrams.should include(group)
     end
   end
-    
+
 end
