1c6911e3667117ab560e0e91cbcb6b22ba9e7b1a
nikita <nikita_0208@tamu.edu>
2015-09-13 21:30:16 -0500

    attr_accessor_history passes 3 tests, only :(

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -6,9 +6,9 @@ class RockPaperScissors
   def self.winner(player1, player2)
     # Determines the winner among the two players.
     allowed_strategy = ["R", "P", "S"]
-    if !(allowed_strategy.include?(player1[1]) && allowed_strategy.include?(player2[1]))
-      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
-    else
+     if !(allowed_strategy.include?(player1[1]) && allowed_strategy.include?(player2[1]))
+       raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+     else
       if player1[1] == player2[1]
         return player1
       end
@@ -19,18 +19,15 @@ class RockPaperScissors
       else
         return player2
       end
-    end
+     end
   end
 
   def self.tournament_winner(tournament)
     # takes in an array of games and returns the winner of the tournament.
-    if tournament.length == 2
-      return self.winner(tournament[0], tournament[1])
-    else
-      game_winner1 = self.tournament_winner(tournament[0])
-      game_winner2 = self.tournament_winner(tournament[1])
-      return self.winner(game_winner1, game_winner2)
+    if tournament[0][0].is_a?Array
+      return self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
     end
+    return self.winner(tournament[0], tournament[1])
   end
 
 end
